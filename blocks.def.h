07aadb49fb825d9ef624faff15ccca0bcb4da447
aniketkharel <aniketkhareldev@gmail.com>
2023-06-13 17:24:33 +0545

    added cpu

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Cpu:", "~/dot.org/.config/i3/scripts/cpu_usage",	5,		0},
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"BAT:", "~/dot.org/.config/i3/scripts/battery1", 30, 0 },
 	{"", "~/dot.org/.config/i3/scripts/temperature", 30, 0 },
